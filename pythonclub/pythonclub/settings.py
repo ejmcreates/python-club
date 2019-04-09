8a6cdc1adec5d91413f83060cc796ec786a7c354
ejmcreates <44686503+ejmcreates@users.noreply.github.com>
2019-04-09 12:01:33 -0700

    2nd commit-db configuration and urls.py

--- pythonclub/pythonclub/settings.py
+++ pythonclub/pythonclub/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'clubapp',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'pythonclub.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'pythonclubdb',
+        'USER':'postgres',
+        'PASSWORD':'P@ssw0rd1',
+        'HOST': 'localhost',
+        'PORT':'',
     }
 }
 
