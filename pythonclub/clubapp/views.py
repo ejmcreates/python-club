aed0b09c41bef6d587aa3ecec2fd29c405729f87
ejmcreates <44686503+ejmcreates@users.noreply.github.com>
2019-04-25 11:44:05 -0700

    5-Creates a view to display resource data

--- pythonclub/clubapp/views.py
+++ pythonclub/clubapp/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import Meeting, MeetingMinutes, Event, Resource
 
 # Create your views here.
 def index (request):
     return render(request, 'clubapp/index.html')
+
+def getinfo(request):
+    resource_list=Resource.objects.all()
+    return render(request, 'clubapp/resources.html', {'resource_list' : resource_list})    
