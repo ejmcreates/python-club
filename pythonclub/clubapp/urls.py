7652ffa6b195a0346fae6768eb61ed797a79ab0f
ejmcreates <44686503+ejmcreates@users.noreply.github.com>
2019-05-09 10:54:16 -0700

    6-Adds meeting view and details

--- pythonclub/clubapp/urls.py
+++ pythonclub/clubapp/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('getinfo/', views.getinfo, name='info'),
+    path('getmeetings/', views.getmeetings, name='meetings'),
+    path('meetingdetails/<int:id>', views.meetingdetails, name='meetingdetails'),
 
 ]
--- pythonclub/clubapp/views.py
+++ pythonclub/clubapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Meeting, MeetingMinutes, Event, Resource
 
 # Create your views here.
@@ -7,4 +7,12 @@ def index (request):
 
 def getinfo(request):
     resource_list=Resource.objects.all()
-    return render(request, 'clubapp/resources.html', {'resource_list' : resource_list})    
+    return render(request, 'clubapp/resources.html', {'resource_list' : resource_list})   
+
+def getmeetings(request):
+    meeting_list=Meeting.objects.all()
+    return render(request, 'clubapp/meetings.html', {'meeting_list': meeting_list})
+
+def meetingdetails(request, id):
+    meet_detail=get_object_or_404(Meeting, pk=id)
+    return render(request, 'clubapp/meetingdetails.html', {'meet_detail': meet_detail})
